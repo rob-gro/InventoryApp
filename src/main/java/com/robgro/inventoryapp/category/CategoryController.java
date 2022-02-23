4600c24af23dd93890552a98531cffa3950e1fa5
RobGro <r.grodzki74@gmail.com>
2022-02-23 19:35:14 +0000

    added new category feature

--- src/main/java/com/robgro/inventoryapp/category/CategoryController.java
+++ src/main/java/com/robgro/inventoryapp/category/CategoryController.java
@@ -1,26 +1,37 @@
 package com.robgro.inventoryapp.category;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
 @Controller
 public class CategoryController {
 
-    @Autowired
     private CategoryRepository categoryRepository;
 
-//    public CategoryController(CategoryRepository categoryRepository) {
-//        this.categoryRepository = categoryRepository;
-//    }
+    public CategoryController(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
 
     @GetMapping("/categories")
     public String showCategoryList(Model model) {
         List<Category> listCategories = categoryRepository.findAll();
         model.addAttribute("listCategories", listCategories);
-    return "categories";
+        return "categories";
+    }
+
+    @GetMapping("/categories/new")
+    public String showCategoryNewForm(Model model) {
+        model.addAttribute("category", new Category());
+        return "category_form";
+    }
+
+    @PostMapping("/categories/save")
+    public String saveCategory(Category category) {
+        categoryRepository.save(category);
+        return "redirect:/categories";
     }
 }
