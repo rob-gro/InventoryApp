f98550ebaa3e53286f5d1266981f82d221777122
RobGro <r.grodzki74@gmail.com>
2022-03-04 23:43:22 +0000

    added cart item as a intermediate table between user and product

---
+++ src/main/java/com/robgro/inventory_app/shopping_cart/CartItem.java
@@ -0,0 +1,73 @@
+package com.robgro.inventory_app.shopping_cart;
+
+import com.robgro.inventory_app.product.Product;
+import com.robgro.inventory_app.user.User;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "cart_items")
+public class CartItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private int quantity;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public CartItem() {
+    }
+
+    public CartItem(int quantity, Product product, User user) {
+        this.quantity = quantity;
+        this.product = product;
+        this.user = user;
+    }
+
+    public CartItem(Integer id, int quantity, Product product, User user) {
+        this.id = id;
+        this.quantity = quantity;
+        this.product = product;
+        this.user = user;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
